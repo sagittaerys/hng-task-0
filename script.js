36d150386dc9c1b54c4d471ff64bc8106ffcc528
Olamilekan Aremu <aremu0235@gmail.com>
2025-10-16 10:04:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateTime() {
+    const timeElement = document.querySelector('[data-testid="test-user-time"]');
+    if (timeElement) {
+        
+        timeElement.textContent = Date.now();
+    }
+    
+    
+}
+
+updateTime();
+setInterval(updateTime, 1000);
